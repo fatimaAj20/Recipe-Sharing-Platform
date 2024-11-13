9459c0da36076271e2d015ab042d1b1ddb8d010b
Fatima Ajroush <ajfatima41@gmail.com>
2024-11-13 15:55:36 -0800

    fixing error

--- RecipeSharingProject.API/Controllers/RecipeController.cs
+++ RecipeSharingProject.API/Controllers/RecipeController.cs
@@ -12,12 +12,10 @@ public class RecipeController: ControllerBase
 {
     private IRecipeService RecipeService { get; }
 
-    private ILogger<RecipeController> Logger { get; }
 
-    public RecipeController(IRecipeService recipeService, ILogger<RecipeController> logger)
+    public RecipeController(IRecipeService recipeService)
     {
         RecipeService = recipeService;
-        this.Logger = logger;
     }
 
     [HttpPost]
@@ -56,20 +54,10 @@ public class RecipeController: ControllerBase
     [Route("Get")]
     public async Task<IActionResult> GetRecipes([FromQuery] RecipeFilter recipeFilter)
     {
-        Logger.LogInformation($"Get Recipes called with email: {recipeFilter.Email}");
-
-        try
-        {
-            throw new Exception("This is a test exception");
-            var recipes = await RecipeService.GetRecipesAsync(recipeFilter);
-            return Ok(recipes);
-        }
-        catch(Exception ex)
-        {
-            Logger.LogError(ex, "Error happened when getting recipes");
-            return Ok();
-        }
+        var recipes = await RecipeService.GetRecipesAsync(recipeFilter);
+        return Ok(recipes);
     }
+
     [HttpGet]
     [Route("Share/{sharingKey}")]
     public async Task<IActionResult> ShareRecipe(string sharingKey)
